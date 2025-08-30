8f69b9cf55536dd54f17619a382dfc6a78705cf0
spazemail <spazemail.ai@gmail.com>
2025-08-30 16:36:28 +0530

    Update iothub.tf

--- infra/iothub.tf
+++ infra/iothub.tf
@@ -1,21 +1,12 @@
-# Create a custom Event Hub endpoint on the IoT Hub
-resource "azurerm_iothub_endpoint_eventhub" "eh_ep" {
-  resource_group_name = var.resource_group
-  iothub_id           = data.azurerm_iothub.iothub.id
-  name                = "SqlIngestionToEventHub"   # <-- your custom name
-  endpoint_uri        = "sb://${data.azurerm_eventhub_namespace.eventhubs_namespace.name}.servicebus.windows.net"
-  entity_path         = azurerm_eventhub.eventhub_driver_messages.name
-  authentication_type = "identityBased"
-}
 
-# Route to that custom endpoint
-resource "azurerm_iothub_route" "to_custom" {
+# Route DeviceMessages (Raw) to the built-in EH-compatible endpoint
+resource "azurerm_iothub_route" "to_builtin_events" {
   resource_group_name = var.resource_group
   iothub_name         = data.azurerm_iothub.iothub.name
-  name                = "SqlIngestionToEH"
+  name                = "SqlIngestionToBuiltInEvents"
 
   source         = "DeviceMessages"
   condition      = "$body.MessageType = 'Raw'"
-  endpoint_names = [azurerm_iothub_endpoint_eventhub.eh_ep.name]  # "SqlIngestionToEventHub"
+  endpoint_names = ["events"]   # built-in Event Hubs–compatible endpoint
   enabled        = true
 }
