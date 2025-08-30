a843fc4574f5ff16b0c14638040ba2ce67d373de
spazemail <spazemail.ai@gmail.com>
2025-08-30 19:02:15 +0530

    Update iothub.tf

--- infra/iothub.tf
+++ infra/iothub.tf
@@ -1,23 +1,30 @@
-# IoT Hub -> Event Hub endpoint (keyBased)
-resource "azurerm_iothub_endpoint_eventhub" "iothub_endpoint_eventhub_messages" {
-  resource_group_name = var.resource_group
-  iothub_id           = data.azurerm_iothub.iothub.id
-  name                = "EventHubMessages2"
-
-  endpoint_uri        = "sb://${data.azurerm_eventhub_namespace.eventhubs_namespace.name}.servicebus.windows.net"
-  entity_path         = azurerm_eventhub.eventhub_driver_messages.name
-
-  authentication_type = "keyBased"
-  connection_string   = azurerm_eventhub_namespace_authorization_rule.ehns_send.primary_connection_string
+# Role: Azure Event Hubs Data Sender
+resource "azurerm_role_assignment" "iothub_eh_sender" {
+  scope                = data.azurerm_eventhub.driver_messages.id   # or the namespace id
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = azurerm_iothub.iothub.identity[0].principal_id
 }
 
+# 3) Identity-based endpoint (NO connection_string)
+resource "azurerm_iothub_endpoint_eventhub" "iothub_endpoint_eventhub_messages" {
+  resource_group_name = var.resource_group
+  iothub_id           = azurerm_iothub.iothub.id
+  name                = "EventHubMessages2"
+
+  endpoint_uri        = "sb://${data.azurerm_eventhub_namespace.ehns.name}.servicebus.windows.net"
+  entity_path         = azurerm_eventhub.eventhub_driver_messages.name
+
+  authentication_type = "identityBased"
+}
+
+# 4) Route to that endpoint
 resource "azurerm_iothub_route" "telemetry_to_custom_eventhub" {
   resource_group_name = var.resource_group
-  iothub_name         = data.azurerm_iothub.iothub.name
-  name                = "TelemetryToEventHub"
+  iothub_name         = azurerm_iothub.iothub.name
+  name                = "SqlIngestionToBuiltInEvents"
 
   source         = "DeviceMessages"
-  condition      = "$body.MessageType = 'TelemetryData'"
+  condition      = "$body.MessageType = 'Raw'"
   endpoint_names = [azurerm_iothub_endpoint_eventhub.iothub_endpoint_eventhub_messages.name]
   enabled        = true
 
