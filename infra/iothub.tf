86fc7d5308f32f99f7138e10a8ae933b16abacce
spazemail <spazemail.ai@gmail.com>
2025-08-30 13:26:24 +0530

    Update iothub.tf

--- infra/iothub.tf
+++ infra/iothub.tf
@@ -1,12 +1,11 @@
 # Route DeviceMessages ('Raw') to the EXISTING custom EH endpoint
-
-resource "azurerm_iothub_route" "sql_ingestion_to_eventhub" {
+resource "azurerm_iothub_route" "to_builtin_events" {
   resource_group_name = var.resource_group
   iothub_name         = data.azurerm_iothub.iothub.name
   name                = "SqlIngestionToEventHub2"
 
   source         = "DeviceMessages"
-  condition      = "$body.MessageType = 'Raw'"
-  endpoint_names = ["SqlIngestionToEventHub"]  # exact name from the portal
+  condition      = "$body.MessageType = 'Raw'"   # or just "true" to test
+  endpoint_names = ["events"]                     # built-in endpoint name is EXACTLY "events"
   enabled        = true
 }
