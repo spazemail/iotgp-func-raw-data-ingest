4e6d2192a2e7a1ebcbd644080e3d406e2b5a22a5
spazemail <spazemail.ai@gmail.com>
2025-09-02 01:23:41 +0530

    Update iothub.tf

--- infra/iothub.tf
+++ infra/iothub.tf
@@ -1,5 +1,14 @@
 
 # ---------------------------------------------
+# RBAC: IoT Hub MI -> EH Sender
+# ---------------------------------------------
+resource "azurerm_role_assignment" "iothub_eventhub_sender" {
+  scope                = azurerm_eventhub.eventhub_driver_messages.id
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = data.azurerm_iothub.iothub.identity[0].principal_id
+}
+
+# ---------------------------------------------
 # IoT Hub -> Event Hub endpoint (Managed Identity)
 # ---------------------------------------------
 resource "azurerm_iothub_endpoint_eventhub" "iothub_endpoint_eventhub_messages" {
@@ -11,7 +20,9 @@ resource "azurerm_iothub_endpoint_eventhub" "iothub_endpoint_eventhub_messages" 
   entity_path         = azurerm_eventhub.eventhub_driver_messages.name
   authentication_type = "identityBased"
 
-
+  depends_on = [
+    azurerm_role_assignment.iothub_eventhub_sender
+  ]
 }
 
 # ---------------------------------------------
@@ -20,7 +31,7 @@ resource "azurerm_iothub_endpoint_eventhub" "iothub_endpoint_eventhub_messages" 
 resource "azurerm_iothub_route" "telemetry_to_custom_eventhub" {
   resource_group_name = var.resource_group
   iothub_name         = data.azurerm_iothub.iothub.name
-  name                = "SqlIngestionToEvents2
+  name                = "SqlIngestionToEvents2-Route"
 
   source         = "DeviceMessages"
   condition      = "$body.MessageType = 'Raw'"
